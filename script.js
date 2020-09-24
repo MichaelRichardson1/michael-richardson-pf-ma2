f32a9a7ab6390be1098dfef1a3b03d1a373b935e
MichaelRichardson <michael.hu.richardson@gmail.com>
2020-09-24 08:37:52 +0200

    Module Assignment 2 - Javascript

---
+++ script.js
@@ -0,0 +1,55 @@
+/* MA 2 */
+
+// question 1
+
+var petArray = ["cat","cow","dog"];
+
+// question 2
+
+console.log(petArray[1]);
+
+// question 3
+
+petArray.push("sheep");
+
+// question 4
+
+console.log(petArray.length);
+
+// question 5
+
+var catObject = {
+	name: "Millie",
+	colour: "Brown",
+	age: 24
+	};
+
+// question 6
+
+console.log(catObject.age);
+
+// question 7
+
+catArray = [
+	{
+	name: "Millie",
+	colour: "Brown",
+	age: 24
+	}
+	];
+
+// question 8
+
+for (var i = 0; i < catArray.length; i++) {
+	console.log(catArray[i]);
+}
+
+// question 9
+
+function logToConsole(animalType) {
+	console.log(animalType);
+}
+
+// question 10
+
+logToConsole("donkey")
